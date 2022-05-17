c665f74efc7dbcf6bd87a526e1c35307dee95dc4
Atsushi <akakak0002@gmail.com>
2022-05-17 12:36:25 +0900

    fix

--- dynamo_db.rb
+++ dynamo_db.rb
@@ -14,17 +14,17 @@ require 'aws-sdk-dynamodb'
 # puts client.scan(table_name: "amazon_item_list").to_a
 
 class DynamoDb
-  def initialize
-    @client = Aws::DynamoDB::Client.new(
+  def all
+    client.scan(table_name: "amazon_item_list")["items"].to_a
+  end
+
+  private
+  
+  def client
+    @client ||= Aws::DynamoDB::Client.new(
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION']
     )
-  end
-
-  attr_reader :client
-
-  def all
-    client.scan(table_name: "amazon_item_list").to_a
   end
 end
--- scraper.rb
+++ scraper.rb
@@ -1,42 +1,114 @@
 # selenium-webdriverを取り込む
 require 'selenium-webdriver'
 require './twitter_api'
+require './dynamo_db'
+require 'parallel'
 
-class Scraper
-  def initialize
+class Crawler
+  CART_SELLER_XPATH = '//*[@id="tabular-buybox"]/div[1]/div[4]/div/span/a'
+                      #  //*[@id="tabular-buybox"]/div[1]/div[4]/div/span/a
+  # CART_SELLER_XPATH = '//*[@id="tabular-buybox"]/div[1]/div[4] | //*[@id="tabular-buybox"]/div[1]/div[4]/div/span/a'
+  # //*[@id="tabular-buybox"]/div[1]/div[4]/div
+  CART_PRICE_XPATH = '//*[@id="corePrice_desktop"]/div/table/tbody/tr[2]/td[2]/span[1]/span[1] | //*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span/span[1]'
+  # CART_PRICE_XPATH2 = '//*[@id="corePrice_feature_div"]/div/span[1]/span[2]'
+  NORMAL_ORDER_RADIO_BUTTON_XPATH = '//*[@id="newAccordionRow"]/div/div[1]/a/i'
+  LABEL_XPATH = '//*[@id="newAccordionCaption_feature_div"]/div/span'
+
+  def initialize(start_url:, desired_arrival_amount:, post_content:)
+    @start_url = start_url
+    @desired_arrival_amount = desired_arrival_amount
+    @post_content = post_content
     # ブラウザの指定(Chrome)
-    @session = Selenium::WebDriver.for :chrome
+    @driver = Selenium::WebDriver.for :chrome
     # 10秒待っても読み込まれない場合は、エラーが発生する
-    session.manage.timeouts.implicit_wait = 10
+    driver.manage.timeouts.implicit_wait = 10
   end
 
-  attr_reader :session
+  # //*[@id="corePrice_desktop"]/div/table/tbody/tr[2]/td[2]/span[1]/span[1]
+  # //*[@id="corePriceDisplay_desktop_feature_div"]/div[1]/span/span[1]
 
-  def execute(url)
-    data1 = {desired_arrival_amount: 10000, monitoring_target: "amazon", post_content: "amazonのもの", start_url: "https://www.amazon.co.jp/gp/product/B079MCT7S5/?tag=sakurachecker-22&th=1"}
-    data2 = {desired_arrival_amount: 10000, monitoring_target: "amazon", post_content: "出店業者のもの", start_url: "https://www.amazon.co.jp/%EF%BC%882%E3%82%B1%E3%83%BC%E3%82%B9%EF%BC%89%E6%98%8E%E6%B2%BB-%E3%82%B6%E3%83%90%E3%82%B9-SAVAS-%E3%83%9F%E3%83%AB%E3%82%AF%E3%83%97%E3%83%AD%E3%83%86%E3%82%A4%E3%83%B3-200ml%C3%9724%E6%9C%AC%E5%85%A5%C3%972%E3%82%B1%E3%83%BC%E3%82%B9/dp/B08YNHHTFL?pd_rd_w=cqNWq&pf_rd_p=cfde786c-3796-4675-b27f-944242fa9d28&pf_rd_r=MZS2MS9CMXSZWD00YKQB&pd_rd_r=71645dc5-d3f7-4a30-8329-1e5bd85a4014&pd_rd_wg=N0g9w&pd_rd_i=B08YNHHTFL&psc=1&ref_=pd_bap_d_rp_1_i"}
-    data3 = {desired_arrival_amount: 1, monitoring_target: "amazon", post_content: "出店業者のもの", start_url: "https://www.amazon.co.jp/%EF%BC%882%E3%82%B1%E3%83%BC%E3%82%B9%EF%BC%89%E6%98%8E%E6%B2%BB-%E3%82%B6%E3%83%90%E3%82%B9-SAVAS-%E3%83%9F%E3%83%AB%E3%82%AF%E3%83%97%E3%83%AD%E3%83%86%E3%82%A4%E3%83%B3-200ml%C3%9724%E6%9C%AC%E5%85%A5%C3%972%E3%82%B1%E3%83%BC%E3%82%B9/dp/B08YNHHTFL?pd_rd_w=cqNWq&pf_rd_p=cfde786c-3796-4675-b27f-944242fa9d28&pf_rd_r=MZS2MS9CMXSZWD00YKQB&pd_rd_r=71645dc5-d3f7-4a30-8329-1e5bd85a4014&pd_rd_wg=N0g9w&pd_rd_i=B08YNHHTFL&psc=1&ref_=pd_bap_d_rp_1_i"}
+  attr_reader :start_url, :desired_arrival_amount, :post_content, :driver
 
-    session.navigate.to url
+  def item_meets_condition?
+    puts start_url
+    driver.navigate.to start_url
+    puts "start_url: #{start_url}"
 
-    cart_seller = session.find_element(:xpath, '//*[@id="tabular-buybox"]/div[1]/div[4]').text
-    puts cart_seller
+    # 定期便が存在する商品の場合、通常の注文を選択する
+    # click_normal_order_button
 
+    cart_seller = driver.find_elements(:xpath, CART_SELLER_XPATH)
+    puts cart_seller.class
+    puts cart_seller[0].text
+    # puts cart_seller.value
+    puts cart_seller.inspect
+    puts "cart_seller: #{cart_seller}"
+    
     if cart_seller == "Amazon.co.jp"
-      TwitterApi.new.tweet(data1[:post_content])
-    else
-      cart_price = session.find_element(:xpath, '//*[@id="corePrice_feature_div"]/div/span[1]/span[2]/span[2]').text
-      puts cart_price
+      puts '販売元はAmazon.co.jpです'
+      return true
 
-      if cart_price.to_i <= data2[:desired_arrival_amount]
-        TwitterApi.new.tweet(data2[:post_content])
+    else
+      puts '販売元はAmazon.co.jpではありません'
+
+      cart_price = driver.find_element(:xpath, CART_PRICE_XPATH).text.delete("￥").to_i
+      puts cart_price.class
+
+      if cart_price <= desired_arrival_amount
+        return true
       end
     end
-  rescue => e
-    puts e
+
   ensure
-    session.quit
+    driver.quit
+  end
+
+  private
+
+  def click_normal_order_button
+    radio_button = driver.find_elements(:xpath, NORMAL_ORDER_RADIO_BUTTON_XPATH)[0]
+    radio_button.click if radio_button
+  end
+
+  def logger
+    @logger ||= logger = Logger.new('./logfile.log')
   end
 end
 
-Scraper.new.execute("https://www.amazon.co.jp/%EF%BC%882%E3%82%B1%E3%83%BC%E3%82%B9%EF%BC%89%E6%98%8E%E6%B2%BB-%E3%82%B6%E3%83%90%E3%82%B9-SAVAS-%E3%83%9F%E3%83%AB%E3%82%AF%E3%83%97%E3%83%AD%E3%83%86%E3%82%A4%E3%83%B3-200ml%C3%9724%E6%9C%AC%E5%85%A5%C3%972%E3%82%B1%E3%83%BC%E3%82%B9/dp/B08YNHHTFL?pd_rd_w=cqNWq&pf_rd_p=cfde786c-3796-4675-b27f-944242fa9d28&pf_rd_r=MZS2MS9CMXSZWD00YKQB&pd_rd_r=71645dc5-d3f7-4a30-8329-1e5bd85a4014&pd_rd_wg=N0g9w&pd_rd_i=B08YNHHTFL&psc=1&ref_=pd_bap_d_rp_1_i")
+rows = DynamoDb.new.all
+puts rows[1]["start_url"]
+row = rows[1]
+
+puts "投稿内容: #{row["post_contents"]}"
+# TwitterApi.new.tweet(row["post_contents"]) if crawler.item_meets_condition?
+
+arr = []
+4.times { arr << row[10] }
+puts arr
+
+
+def main
+  tweetable_rows = []
+
+  Parallel.each(arr, in_threads: 5) do |row|
+    crawler = Crawler.new(
+      start_url: row["start_url"],
+      desired_arrival_amount: row["desired_arrival_amount"].to_i,
+      post_content: row["post_contents"]
+    )
+    tweetable_rows << row if crawler.item_meets_condition?
+  end
+
+  if tweetable_rows.any?
+  twitter_api  = TwitterApi.new
+
+  tweetable_rows.each do |row|
+    twitter_api.tweet(row["post_content"])
+    sleep(5)
+  end
+rescue => e
+  puts e
+  puts e.backtrace
+  logger.error e.class
+  logger.error e.message
+end
