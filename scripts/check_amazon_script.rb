81a03b11f5278d8ed80ff1077251d244d1dc577d
Atsushi <akakak0002@gmail.com>
2022-06-05 01:57:43 +0900

    ツイートインターバルを指定できるようにする

--- scripts/check_amazon_script.rb
+++ scripts/check_amazon_script.rb
@@ -8,6 +8,7 @@ class CheckAmazonScript
   TABLE_NAME = 'amazon_item_list'.freeze
   # TWEET_INTERVAL = 1800.freeze # 1800秒
   TWEET_INTERVAL = 0.freeze # 0秒
+  DEFAULT_TWEET_INTERVAL = 1800.freeze # 0秒
 
   def execute
     overall_start_time = Time.now # 全体時間測定
@@ -19,7 +20,7 @@ class CheckAmazonScript
 
     target_rows = []
     Parallel.each(rows, in_threads: 5) do |row|
-      start_time = Time.now # 時間測定
+      start_time = Time.now # 個別時間測定
   
       scenaio = Crawler::Amazon::Scenario.new(
         start_url: row["start_url"],
@@ -31,20 +32,17 @@ class CheckAmazonScript
       p "個別処理概要 #{Time.now - start_time}s" # 個別時間測定
     end
   
-    if target_rows.any?
-      target_rows.each do |row|
-        if tweetable?(row)
-          p "ツイートします"
-          # p row["post_contents"]
-          twitter_api.tweet(row["post_contents"])
-          dynamo_db.update(id: row["id"], column: "last_tweeted_at", value: Time.now.to_s)
-          # sleep(rand(20..30))
-        end
+    target_rows.each do |row|
+      if tweetable?(row)
+        p "ツイートします"
+        # p row["post_contents"]
+        # twitter_api.tweet(row["post_contents"])
+        dynamo_db.update(id: row["id"], column: "last_tweeted_at", value: Time.now.to_s)
+        # sleep(rand(20..30))
       end
-  
-      p "全体処理概要 #{Time.now - overall_start_time}s" # 時間測定
     end
   
+    p "全体処理概要 #{Time.now - overall_start_time}" # 全体時間測定
   rescue => e
     ErrorUtility.log(e)
   end
@@ -52,14 +50,16 @@ class CheckAmazonScript
   private
 
   def tweetable?(row)
-    return true if row["last_tweeted_at"] == nil # last_tweeted_atがnilの場合はツイート可能
+    return true if row["last_tweeted_at"].nil? # last_tweeted_atがnilの場合はツイート可能
 
     last_tweeted_at = Time.parse(row["last_tweeted_at"])
-    Time.now > last_tweeted_at + TWEET_INTERVAL
+    # tweet_intervalがnilの場合はデフォルトの30分、tweet_intervalが存在する場合は指定の値
+    tweet_interval = row["tweet_interval"].nil? ? DEFAULT_TWEET_INTERVAL : row["tweet_interval"].to_i
+    Time.now > last_tweeted_at + tweet_interval
   end
 
   def dynamo_db
-    @dynamo_db ||= DynamoDb.new(TABLE_NAME)
+    dynamo_db ||= DynamoDb.new(TABLE_NAME)
   end
 
   def twitter_api
