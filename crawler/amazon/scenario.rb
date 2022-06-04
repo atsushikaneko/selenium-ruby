e39637eb1aee048d2496d41fb7207bc9c8d20307
Atsushi <akakak0002@gmail.com>
2022-06-04 11:44:56 +0900

    リファクタ

--- crawler/amazon/scenario.rb
+++ crawler/amazon/scenario.rb
@@ -12,41 +12,32 @@ module Crawler
         @start_url = start_url
         @desired_arrival_amount = desired_arrival_amount
         @post_content = post_content
-        # ヘッドレスモードの場合以下コメントイン
-        options = Selenium::WebDriver::Chrome::Options.new
-        options.add_argument('--headless')
-        @driver = Selenium::WebDriver.for :chrome , options: options
-        # ヘッドレスモードじゃない場合は以下コメントイン
-        # @driver = Selenium::WebDriver.for :chrome
-        # driver.manage.timeouts.implicit_wait = 10 # 10秒待っても読み込まれない場合は、エラーが発生する
       end
     
       attr_reader :start_url, :desired_arrival_amount, :post_content, :driver
     
       def item_in_stock_by_target_sellers?
-        driver.navigate.to start_url
         puts "start_url: #{start_url}"
+        driver.navigate.to start_url
     
         # 定期便が存在する商品の場合、通常の注文を選択する
         click_normal_order_button
         # クリックしたあと少し待つ
         sleep(1)
     
-        return false unless cart_seller = driver.find_elements(:xpath, CART_SELLER_XPATH)[0]&.text
-        puts "cart_seller: #{cart_seller}"
+        # カートセラーが取得できない場合はfalseを返す
+        return false unless cart_seller_name = driver.find_elements(:xpath, CART_SELLER_XPATH)[0]&.text
+        puts "cart_seller_name: #{cart_seller_name}"
         
-        # カートセラーがAmazonの場合はtrue
-        # カートセラーがAmazon意外の場合は、指定価格以下ならtrue
-        if cart_seller == "Amazon.co.jp"
+        # カートセラーがAmazonの場合はtrueを返す
+        # カートセラーがAmazon以外の場合は、指定価格以下ならtrueを返す
+        if cart_seller_name == "Amazon.co.jp"
           puts '販売元はAmazon.co.jpです'
           return true
         else
           puts '販売元はAmazon.co.jpではありません'
           cart_price = driver.find_element(:xpath, CART_PRICE_XPATH).text.delete("￥").to_i
-          puts cart_price.class
-          if cart_price <= desired_arrival_amount
-            return true
-          end
+          return true if cart_price <= desired_arrival_amount
         end
 
       ensure
@@ -54,6 +45,18 @@ module Crawler
       end
     
       private
+
+      def driver
+        @driver ||= begin
+          # ヘッドレスモードの場合以下コメントイン
+          options = Selenium::WebDriver::Chrome::Options.new
+          options.add_argument('--headless')
+          Selenium::WebDriver.for :chrome , options: options
+          # ヘッドレスモードじゃない場合は以下コメントイン
+          # @driver = Selenium::WebDriver.for :chrome
+          # driver.manage.timeouts.implicit_wait = 10 # 10秒待っても読み込まれない場合は、エラーが発生する
+        end
+      end
     
       def click_normal_order_button
         radio_button = driver.find_elements(:xpath, NORMAL_ORDER_RADIO_BUTTON_XPATH)[0]
